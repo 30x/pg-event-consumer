aa9c9b1f17aafc8bca1cfa177144d7780a36fa97
Martin Nally <mnally@google.com>
2017-02-01 11:03:25 -0800

    remove ;

--- pg-event-consumer-pg.js
+++ pg-event-consumer-pg.js
@@ -11,48 +11,48 @@ var config = {
 var pool = new Pool(config)
 
 function registerConsumer(ec) {
-  var time = Date.now();
+  var time = Date.now()
   var query = 'INSERT INTO consumers (ipaddress, registrationtime) values ($1, $2) ON CONFLICT (ipaddress) DO UPDATE SET registrationtime = EXCLUDED.registrationtime'
   pool.query(query, [ec.ipaddress, time], function (err, pgResult) {
     if (err) 
       console.log(`unable to register ipaddress ${ec.ipaddress} ${err}`)
     else
       console.log(`registered consumer ${ec.ipaddress} at time ${time}`)
-  });
+  })
 }
 
 function withEventsAfter(index, callback) {
-  var query = 'SELECT * FROM events WHERE index > $1 ORDER BY index';
+  var query = 'SELECT * FROM events WHERE index > $1 ORDER BY index'
   pool.query(query, [index], function(err, pgResult) {
     if (err)
-      console.log(`unable to retrieve events subsequent to ${index} ${err}`);      
+      console.log(`unable to retrieve events subsequent to ${index} ${err}`)      
     else {
-      console.log(`retrieved events subsequent to ${index}`);      
-      callback(pgResult.rows);
+      console.log(`retrieved events subsequent to ${index}`)      
+      callback(pgResult.rows)
     }
-  });
+  })
 }
 
 function withLastEventID(callback) {
   var query = 'SELECT last_value FROM events_index_seq'
   pool.query(query, function(err, pgResult) {
     if(err) {
-      console.log('error retrieving last event ID', err);
-      callback(err);
+      console.log('error retrieving last event ID', err)
+      callback(err)
     } else {
-      console.log('retrieved last event ID', pgResult.rows[0].last_value);
-      callback(null, parseInt(pgResult.rows[0].last_value));
+      console.log('retrieved last event ID', pgResult.rows[0].last_value)
+      callback(null, parseInt(pgResult.rows[0].last_value))
     }
   })
 }
 
 (function init() {
-  var query = 'CREATE TABLE IF NOT EXISTS events (index bigserial, topic text, eventtime bigint, data jsonb)';
+  var query = 'CREATE TABLE IF NOT EXISTS events (index bigserial, topic text, eventtime bigint, data jsonb)'
   pool.query(query, function(err, pgResult) {
     if(err) 
       console.error('error creating events table', err)
     else {
-      query = 'CREATE TABLE IF NOT EXISTS consumers (ipaddress text primary key, registrationtime bigint)';
+      query = 'CREATE TABLE IF NOT EXISTS consumers (ipaddress text primary key, registrationtime bigint)'
       pool.query(query, function(err, pgResult) {
       if(err) 
         console.error('error creating consumers table', err)
